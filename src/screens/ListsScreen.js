d22149a68f38a17958f007ff50e6daf6ffce542c
magluf <smagluf@gmail.com>
2019-11-18 16:44:51 -0300

    original files

---
+++ src/screens/ListsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ListsScreen = () => {
+  return <Text>Lists Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ListsScreen;
